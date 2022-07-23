c700af94ae448542224f912df020fecf6fb3379f
Дмитрий Киселев <dmit-kiselyov3105@yandex.ru>
2022-07-23 23:04:35 +0300

    23.22

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -13,39 +13,42 @@ CUTOFF = 10
 User = get_user_model()
 
 
-def get_page_context(queryset, request):
-    paginator = Paginator(queryset, CUTOFF)
+def index(request):
+    posts = Post.objects.select_related('group')
+    paginator = Paginator(posts, CUTOFF)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return {
-        'paginator': paginator,
-        'page_number': page_number,
+    context = {
+        'posts': posts,
         'page_obj': page_obj,
     }
-
-
-def index(request):
-    context = get_page_context(Post.objects.all(), request)
     return render(request, 'posts/index.html', context)
 
 
 def group_list(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:CUTOFF]
+    posts = group.posts.all()
+    paginator = Paginator(posts, CUTOFF)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
         'posts': posts,
+        'page_obj': page_obj,
     }
-    context.update(get_page_context(group.posts.all(), request))
     return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    paginator = Paginator(posts, CUTOFF)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'author': author,
+        'page_obj': page_obj,
     }
-    context.update(get_page_context(author.posts.all(), request))
     return render(request, 'posts/profile.html', context)
 
 
